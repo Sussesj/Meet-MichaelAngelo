f52fc09aafd5eae21ae3317f63523ecdbcbef47a
Sussesj <sussesj@gmail.com>
2013-09-11 14:53:15 -0400

    Create

---
+++ script.js
@@ -0,0 +1,52 @@
+// Check if the user is ready to play!
+
+//var numberOfMen=4;
+// var person="John Doe";
+// var answer='Yes I am!';
+// 
+// 
+// document.write(person + "<br>");
+// document.write(answer + "<br>");	
+ 
+confirm("The Young Archer takes multiple characters!");
+ 
+document.write("He takes the shape of four characters<br>"); 
+
+confirm("You want to know who those four characters are?");
+
+document.write("<strong>A</strong> The Young Archer<br>");
+document.write("<strong>B</strong> The Marble Boy<br>");
+document.write("<strong>C</strong> The Lost Cupid<br>");
+document.write("<strong>D</strong> The neglected Child<br>");
+ 
+ var archer = prompt("choose your character, a,b,c or d");
+//  if(archer ==== a){
+//  	document.write("He is One fusty thing<br>");
+//  }
+//  else if (archer === b){
+//  	document.write("He is pretty doll<br>");
+//  }
+//  else if (archer === c){
+//     document.write("Fuck No!<br>");
+//  }
+// else (archer === d){
+// 	document.write("Hell yeah<br>");
+// };
+// document.write("<p>Snow White and Batman were hanging out at the bus stop, waiting to go to the shops. There was a sale on and both needed some new threads. You've never really liked Batman. You walk up to him."</p>");
+// 
+// console.log("Batman glares at you.");
+// var userAnswer = prompt("Are you feeling lucky, punk?");
+// if(userAnswer === "yes"){
+//     console.log("Batman hits you very hard. It's Batman and you're you! Of course Batman wins!")
+// }
+// else{
+//     console.log("You did not say yes to feeling lucky. Good choice! You are a winner in the game of not getting beaten up by Batman.")
+// }
+// var feedback = prompt("What did you think, put in a number from 1 to 10?");
+// if(feedback>8){
+//     console.log("This is just the begining of my game empire. Stay tuned for more!")
+// }
+// else{
+//     console.log("I slaved away at this game and you gave me that score?! The nerve! Just you wait!")
+// }
+// -->
